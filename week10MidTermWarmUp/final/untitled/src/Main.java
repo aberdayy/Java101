5ba6b1d1fe70db90f727716c3cb9c1297331bd96
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-05-25 00:05:48 +0300

    UPDATE

---
+++ week10MidTermWarmUp/final/untitled/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+//Problem => Write a java game where program asks user how many numbers he/she will enter, then asks and accepts that many numbers. Finally, prints all the numbers user entered backwards.
+//upload your code as a single text file.
+
+
+
+    }
+}
---
+++ week10MidTermWarmUp/untitled1/src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+
+    }
+}
